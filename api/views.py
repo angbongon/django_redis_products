acd7820fcf924ae85f283a2c4ced469f2da521ab
Ángel Bonilla González <angbongon@alum.us.es>
2021-10-06 15:54:24 +0200

    [feat](views) Adds comments for docs

--- api/views.py
+++ api/views.py
@@ -7,6 +7,25 @@ from api.permissions import OrderPermission
 
 
 class OrderViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return an order instance.
+
+    list:
+        Return logged user's order. In case of superuser returns all orders
+
+    create:
+        Create a new order.
+
+    delete:
+        Remove an existing order.
+
+    partial_update:
+        Update one or more fields on an existing order.
+
+    update:
+        Update an order.
+    """
     permission_classes = [IsAuthenticated, OrderPermission]
 
     def get_queryset(self):
@@ -22,5 +41,24 @@ class OrderViewSet(viewsets.ModelViewSet):
 
 
 class ProductViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+        Return an product instance.
+
+    list:
+        Return all products
+
+    create:
+        Create a new product.
+
+    delete:
+        Remove an existing product.
+
+    partial_update:
+        Update one or more fields on an existing product.
+
+    update:
+        Update an product.
+    """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
