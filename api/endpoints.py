60f2afd5caf98d28ae5bf8fef0d0b18604ee90a5
Ángel Bonilla González <angbongon@alum.us.es>
2021-10-06 15:20:20 +0200

    [feat](endpoints) Adds endpoint for api docs

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from django.views.generic import TemplateView
+
+from rest_framework.schemas import get_schema_view
 from rest_framework.routers import DefaultRouter
 
 from api import views
@@ -7,7 +10,12 @@ router = DefaultRouter()
 router.register(r"orders", views.OrderViewSet, basename='orders')
 router.register(r"products", views.ProductViewSet, basename='products')
 
+
 urlpatterns = [
-    path(r"", include(router.urls)),
-    path(r"auth/", include("rest_auth.urls")),
+    path(r'', include(router.urls)),
+    path(r'auth/', include('rest_auth.urls')),
+    path(r'api_schema', get_schema_view(title='Django Products API',
+         description='An API', version='1.0.0'), name='api_schema'),
+    path(r'docs/', TemplateView.as_view(template_name='docs.html',
+         extra_context={'schema_url': 'api_schema'}), name='docs')
 ]
