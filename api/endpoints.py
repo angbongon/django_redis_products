4d21aad363442a8af46f38c80c4a095e088beafa
Ángel Bonilla González <angbongon@alum.us.es>
2021-10-06 15:53:43 +0200

    [refactor](endpoints) Changes swwager for redoc docs

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from django.views.generic import TemplateView
 
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
+
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from api import views
 
@@ -10,12 +12,25 @@ router = DefaultRouter()
 router.register(r"orders", views.OrderViewSet, basename='orders')
 router.register(r"products", views.ProductViewSet, basename='products')
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django Products API",
+        default_version='v1',
+        description="Django Products API docs",
+        contact=openapi.Contact(email="angbonillagonzalez@gmail.com"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[]
+)
 
 urlpatterns = [
     path(r'', include(router.urls)),
-    path(r'auth/', include('rest_auth.urls')),
-    path(r'api_schema', get_schema_view(title='Django Products API',
-         description='An API', version='1.0.0'), name='api_schema'),
-    path(r'docs/', TemplateView.as_view(template_name='docs.html',
-         extra_context={'schema_url': 'api_schema'}), name='docs')
+    path(r'auth/$', include('rest_auth.urls')),
+    # path(r'swagger(?P<format>\.json|\.yaml)',
+    #     schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path(r'swagger/', schema_view.with_ui('swagger',
+    #     cache_timeout=0), name='schema-swagger-ui'),
+    path(r'docs/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
